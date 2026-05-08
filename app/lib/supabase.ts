238dd8a5927e904f9da18b09673b1c282f791ba1
DefiéndeteTú <defiendetetu0@gmail.com>
2026-05-08 14:50:05 -0500

    Delete app/lib/supabase.ts

--- app/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://uabstzfqplwcjxtrpixt.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_CW6BWISbeAGybX7tixHq2Q_rLSAvTRO';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
